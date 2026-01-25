e1979193c12778c4229f95a25129ea30bc0b4df7
Guna Sai <241310022@nitdelhi.ac.in>
2026-01-25 17:34:40 +0530

    fix css import case issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import api from "../services/api";
-import "./Auth.css";
+import "./auth.css";
 
 export default function Login({ setIsAuth }) {
   const [form, setForm] = useState({ username: "", password: "" });
